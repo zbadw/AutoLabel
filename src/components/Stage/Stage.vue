5e403da3ae49fd329efe5208664a8254154516a6
DW <291412082@qq.com>
2023-04-15 00:02:18 +0800

    小修改

--- src/components/Stage/Stage.vue
+++ src/components/Stage/Stage.vue
@@ -1,58 +1,71 @@
 <template>
-  <!-- 上传图片 -->
-  <div id="upload">
-    <input type="file" id="file" accept="image/*" @change="upload" />
-  </div>
   <div id="container">
-    <v-stage :width="width" :height="height">
-      <v-layer style="display: flex; justify-content: center;">
-        <v-image
-          :config="{ x: (width - image_width) / 2, y: (height - image_height) / 2, width: image_width, height: image_height, image: image }" />
+    <v-stage ref="v_stage" :width="width" :height="height">
+      <v-layer ref="v_layer" style="display: flex; justify-content: center;">
+        <v-image ref="v_image" v-if="image" @dragstart="handleDragStart" @dragend="handleDragEnd"
+          :config="{ draggable: true, x: (width - image_width) / 2, y: (height - image_height) / 2, width: image_width, height: image_height, image: image }"
+          @mousemove="position" />
       </v-layer>
     </v-stage>
   </div>
 </template>
 <script setup lang="ts">
 import { onMounted, ref, watch } from "vue";
-const props = defineProps<{ width: number; height: number }>();
-const image = ref<HTMLImageElement | null>(null);
-const image_width = ref(0);
-const image_height = ref(0);
-const scale = ref(1);
-const upload = () => {
-  const file = document.getElementById("file") as HTMLInputElement;
-  const fileObj = file.files?.[0] as Blob;
-  const reader = new FileReader();
-  reader.readAsDataURL(fileObj);
-  reader.onload = function (e) {
-    const url = e.target?.result;
-    image.value = new Image();
-    image.value.src = url as string;
-    image.value.onload = function () {
-      if (image.value instanceof HTMLImageElement) {
-        const { width: imgWidth, height: imgHeight } = image.value;
-        if (imgHeight > props.height || imgWidth > props.width) {
-          scale.value = Math.min(props.height / imgHeight, props.width / imgWidth);
-        } else {
-          scale.value = 1;
-        }
-        image_width.value = imgWidth * scale.value;
-        image_height.value = imgHeight * scale.value;
-        console.log(`${image_width.value}, ${image_height.value}`);
+const props = defineProps<{ width: number; height: number; image: HTMLImageElement }>();
+let hoverTimer: any = null;
+const v_stage = ref<any>(null);
+const v_image = ref<any>(null);
+const v_layer = ref<any>(null);
+let image_scale = ref(1);
+let image_width = ref(0);
+let image_height = ref(0);
+watch(
+  () => props.image,
+  (newVal) => {
+    if (newVal instanceof HTMLImageElement) {
+      const { width: imgWidth, height: imgHeight } = newVal;
+      if (imgHeight > props.height || imgWidth > props.width) {
+        image_scale.value = Math.min(props.height / imgHeight, props.width / imgWidth);
+      } else {
+        image_scale.value = 1;
       }
-      else {
-        console.warn('image.value is not HTMLImageElement');
-      }
-    };
-  };
+      image_width.value = imgWidth * image_scale.value;
+      image_height.value = imgHeight * image_scale.value;
+      console.log(`${image_width.value}, ${image_height.value}`);
+    } else {
+      console.warn("image.value is not HTMLImageElement");
+    }
+  }
+);
+const position = () => {
+  if (hoverTimer) {
+    clearTimeout(hoverTimer);
+  }
+  hoverTimer = setTimeout(function () {
+    const mousePos = v_stage.value.getNode().getPointerPosition();
+    const imageRect = v_image.value.getNode().getClientRect();
+    // 将画布坐标转换为图片坐标
+    const imagePos = v_layer.value.getNode().getAbsoluteTransform().invert().point(mousePos);
+    if (imagePos.x < imageRect.x || imagePos.x > imageRect.x + imageRect.width || imagePos.y < imageRect.y || imagePos.y > imageRect.y + imageRect.height) {
+      return;
+    }
+    console.log('Mouse position in image: (' + ((imagePos.x - imageRect.x) / image_scale.value) + ', ' + ((imagePos.y - imageRect.y) / image_scale.value) + ')');
+  }, 50);
+};
+
+const handleDragStart = () => {
+
+};
+const handleDragEnd = () => {
 };
 </script>
   
 <style scoped>
 #container {
-  border: 1px solid #ccc;
   margin: 0 auto;
   margin-top: 20px;
+  border-radius: 5px;
+  box-shadow: 1px -1px 3px 2px #eee;
 }
 
 #upload {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,18 @@
+<template>
+    <div class="container">
+        <div>
+            <div id="upload">
+                <input type="file" id="file" accept="image/*" @change="upload" />
+            </div>
+            <Stage :width="1000" :height="600" @polygonclick="addLabel" :labels="labels" :image="image" />
+        </div>
+        <div>
+            <ToolBox :width="600" :height="12" />
+            <Label :labels="labels" />
+        </div>
+
+    </div>
+</template>
 <script setup lang="ts">
 import { ref } from "vue";
 import Label from '@/components/Label.vue';
@@ -7,20 +22,22 @@ const labels = ref([]);
 const addLabel = (label: any) => {
     // labels.value.push(label);
 };
+let image = ref<HTMLImageElement | null>(null);
+const upload = () => {
+    const file = document.getElementById("file") as HTMLInputElement;
+    const fileObj = file.files?.[0] as Blob;
+    const reader = new FileReader();
+    reader.readAsDataURL(fileObj);
+    reader.onload = function (e) {
+        const url = e.target?.result;
+        const tempImage = new Image();
+        tempImage.src = url as string;
+        tempImage.onload = function () {
+            image.value = tempImage;
+        };
+    };
+};
 </script>
-
-<template>
-    <div class="container">
-        <div>
-            <Stage :width="1000" :height="600" @polygonclick="addLabel" :labels="labels" />
-        </div>
-        <div>
-            <ToolBox :width="600" :height="12" />
-            <Label :labels="labels"  />
-        </div>
-
-    </div>
-</template>
 
 <style scoped>
 .container {
